4160702e9476858dbcb5f294dc99969f97d6026b
Mounika-bodige <65857924+Mounika-bodige@users.noreply.github.com>
2021-08-08 18:32:25 -0400

    Update Payload.cs

--- CP380-B1-BlockList/Models/Payload.cs
+++ CP380-B1-BlockList/Models/Payload.cs
@@ -8,6 +8,18 @@ namespace CP380_B1_BlockList.Models
 
     public class Payload
     {
-        // TODO
+        public string user { get; set; }
+        public TransactionTypes transactiontype { get; set; }
+        public int amount { get; set; }
+        public string items { get; set; }
+
+
+        public Payload(string username, TransactionTypes transactiontypee, int amount, string items)
+        {
+            this.user = username;
+            this.transactiontype = transactiontypee;
+            this.amount = amount;
+            this.items = items;
+        }
     }
 }
